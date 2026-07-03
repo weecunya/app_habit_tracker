ff70516f675e69064cf498788122f4856648965b
weecunya <wicaszmakowizer@gmail.com>
2026-07-03 14:21:08 +0300

    feat:deployed backend

--- frontend/my-app/vite.config.ts
+++ frontend/my-app/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const proxyTarget = process.env.VITE_PROXY_TARGET ?? "http://localhost:8000";
+const proxyTarget = process.env.VITE_PROXY_TARGET ?? "https://app-habit-tracker.onrender.com/";
 
 export default defineConfig({
   plugins: [react()],
